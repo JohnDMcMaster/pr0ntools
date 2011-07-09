3de1eccdcc1d06d245af92fc2bfa795ce102c817
John McMaster <JohnDMcMaster@gmail.com>
2011-07-09 16:24:17 -0700

    missing header file

---
+++ quietust/files.h
@@ -0,0 +1,23 @@
+/*
+Copyright 2011 Quietust
+Released under 2 clause BSD license, see COPYING for details
+Minor modifications by John McMaster <JohnDMcMaster@gmail.com>
+*/
+
+#ifndef FILES_H
+
+#define DAT_FILE_METAL_VCC			"metal_vcc.dat"
+#define DAT_FILE_METAL_GND			"metal_gnd.dat"
+#define DAT_FILE_METAL				"metal.dat"
+#define DAT_FILE_POLYSILICON		"polysilicon.dat"
+#define DAT_FILE_DIFFUSION			"diffusion.dat"
+#define DAT_FILE_VIAS				"vias.dat"
+#define DAT_FILE_BURIED_CONTACTS	"buried_contacts.dat"
+//not used in DRC
+#define DAT_FILE_TRANSISTORS		"transistors.dat"
+
+#define JS_FILE_TRANSDEFS			"transdefs.js"
+#define JS_FILE_SEGDEFS				"segdefs.js"
+
+#endif
+
