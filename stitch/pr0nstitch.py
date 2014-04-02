c927fbe2971ef1530bc3d8aa7b446c7d04be79bc
John McMaster <JohnDMcMaster@gmail.com>
2014-04-01 21:55:58 -0700

    convert to argparse

--- stitch/pr0nstitch.py
+++ stitch/pr0nstitch.py
@@ -14,16 +14,11 @@ http://uvicrec.blogspot.com/2011/02/scaling-up-image-stitching.html
 
 import sys
 import os.path
-import pr0ntools.pimage
-from pr0ntools.pimage import PImage
-from pr0ntools.pimage import TempPImage
 from pr0ntools.stitch.wander_stitch import WanderStitch
 from pr0ntools.stitch.all_stitch import AllStitch
 from pr0ntools.stitch.grid_stitch import GridStitch
 from pr0ntools.stitch.fortify_stitch import FortifyStitch
-from pr0ntools.execute import Execute
-
-VERSION = '0.1'
+import argparse
 
 project_file = 'panorama0.pto'
 temp_project_file = '/tmp/pr0nstitch.pto'
@@ -39,11 +34,6 @@ grid_only = False
 CONTROL_POINT_ENGINE = AUTOPANO_AJ
 
 def help():
-	print 'pr0nstitch version %s' % VERSION
-	print 'Copyright 2010 John McMaster <JohnDMcMaster@gmail.com>'
-	print 'Usage:'
-	print 'pr0nstitch [args] <files>'
-	print 'files:'
 	print '\timage file: added to input images'
 	print '\tdirectory: added to input image directories'
 	print '\t.pto file: assumed to be project file'
@@ -83,119 +73,79 @@ def arg_fatal(s):
 	help()
 	sys.exit(1)
 
+def t_or_f(arg):
+	arg_value = str(arg).lower()
+	return not (arg_value == "false" or arg_value == "0" or arg_value == "no")
+
+def parser_add_bool_arg(yes_arg, default=False, **kwargs):
+	dashed = yes_arg.replace('--', '')
+	dest = dashed.replace('-', '_')
+	parser.add_argument(yes_arg, dest=dest, action='store_true', default=default, **kwargs)
+	parser.add_argument('--no-' + dashed, dest=dest, action='store_false', **kwargs)
+
 if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description='Stitch images quickly into .pto through hints')
+	parser.add_argument('--out', default='out.pto', help='Output file name')
+	parser_add_bool_arg('--grid-only', default=False, help='')
+	parser.add_argument('--algorithm', default='grid', help='')
+	parser.add_argument('--n-rows', help='')
+	parser.add_argument('--n-cols', help='')
+	parser_add_bool_arg('--alt-rows', default=False, help='')
+	parser_add_bool_arg('--alt-cols', default=False, help='')
+	parser_add_bool_arg('--flip-row', default=False, help='')
+	parser_add_bool_arg('--flip-col', default=False, help='')
+	parser_add_bool_arg('--flip-pre-transpose', default=False, help='')
+	parser_add_bool_arg('--flip-post-transpose', default=False, help='')
+	parser_add_bool_arg('--overwrite', default=False, help='')
+	parser_add_bool_arg('--regular', default=True, help='')
+	parser.add_argument('--x-overlap', help='')
+	parser.add_argument('--y-overlap', help='')
+	parser_add_bool_arg('--dry', default=False, help='')
+	parser_add_bool_arg('--skip-missing', default=False, help='')
+	parser.add_argument('fns', nargs='+', help='File names')
+	args = parser.parse_args()
+
+	depth = 1
+	# CNC like precision?
+	# Default to true for me
+	regular = True
+				
 	input_image_file_names = list()
 	input_project_file_name = None
 	output_project_file_name = None
 	output_image_file_name = None
-	alt_rows = False
-	alt_cols = False
-	n_rows = None
-	n_cols = None
-	flip_col = False
-	flip_row = False
-	flip_pre_transpose = False
-	flip_post_transpose = False
-	depth = 1
-	algorithm = "grid"
-	# CNC like precision?
-	# Default to true for me
-	regular = True
-	x_overlap = None
-	y_overlap = None
-	dry = False
-	skip_missing = False
-	
-	for arg_index in range (1, len(sys.argv)):
-		arg = sys.argv[arg_index]
-		arg_key = None
-		arg_value = None
-		if arg.find("--") == 0:
-			arg_value_bool = True
-			if arg.find("=") > 0:
-				arg_key = arg.split("=")[0][2:]
-				arg_value = arg.split("=")[1]
-				if arg_value == "false" or arg_value == "0" or arg_value == "no":
-					arg_value_bool = False
-			else:
-				arg_key = arg[2:]
-
-			if arg_key == "help":
-				help()
-				sys.exit(0)
-			elif arg_key == "result" or arg_key == "out":
-				if arg_value.find('.pto') > 0:
-					output_project_file_name = arg_value
-				elif PImage.is_image_filename(arg_value):
-					output_image_file_name = arg_value
-				else:
-					arg_fatal('unknown file type %s, use explicit version' % arg)
-			elif arg_key == "grid-only":
-				grid_only = arg_value_bool
-			elif arg_key == "algorithm":
-				algorithm = arg_value
-			elif arg_key == "n-rows":
-				n_rows = int(arg_value)
-			elif arg_key == "n-cols":
-				n_cols = int(arg_value)
-			elif arg_key == "alt-rows":
-				alt_rows = arg_value_bool
-			elif arg_key == "alt-cols":
-				alt_cols = arg_value_bool
-			elif arg_key == "flip-row":
-				flip_row = arg_value_bool
-			elif arg_key == "flip-col":
-				flip_col = arg_value_bool
-			elif arg_key == "flip-pre-transpose":
-				flip_pre_transpose = arg_value_bool
-			elif arg_key == "flip-post-transpose":
-				flip_post_transpose = arg_value_bool
-			elif arg_key == 'no-overwrite':
-				allow_overwrite = not arg_value_bool
-			elif arg_key == 'regular':
-				regular = arg_value_bool
-			elif arg_key == 'skip-missing':
-				skip_missing = arg_value_bool
-			elif arg_key == 'x-overlap':
-				x_overlap = float(arg_value)
-			elif arg_key == 'y-overlap':
-				y_overlap = float(arg_value)
-			elif arg_key == 'dry':
-				dry = True
-			else:
-				arg_fatal('Unrecognized arg: %s' % arg)
+	for arg in args.fns:
+		if arg.find('.pto') > 0:
+			output_project_file_name = arg
+		elif os.path.isfile(arg) or os.path.isdir(arg):
+			input_image_file_names.append(arg)
 		else:
-			if arg.find('.pto') > 0:
-				output_project_file_name = arg
-			elif os.path.isfile(arg) or os.path.isdir(arg):
-				input_image_file_names.append(arg)
-			else:
-				arg_fatal('unrecognized arg: %s' % arg)
+			arg_fatal('unrecognized arg: %s' % arg)
 
 	print 'post arg'
 	print 'output image: %s' % output_image_file_name
 	print 'output project: %s' % output_project_file_name
 	
-	if algorithm == "grid":
+	if args.algorithm == "grid":
 		'''
 		Probably most intuitive is to have (0, 0) at lower left 
 		like its presented in many linear algebra works and XY graph
 		...but image stuff tends to to upper left, so thats what things use
 		'''
-		if n_rows or n_cols:
-			engine = GridStitch.from_file_names(input_image_file_names, flip_col, flip_row, flip_pre_transpose, flip_post_transpose, depth,
-					alt_rows, alt_cols, n_rows, n_cols)
+		if args.n_rows or args.n_cols:
+			engine = GridStitch.from_file_names(input_image_file_names, args.flip_col, args.flip_row, args.flip_pre_transpose, args.flip_post_transpose, depth,
+					args.alt_rows, args.alt_cols, args.n_rows, args.n_cols)
 		else:
 			engine = GridStitch.from_tagged_file_names(input_image_file_names)
-		engine.skip_missing = skip_missing
+		engine.skip_missing = args.skip_missing
 		if grid_only:
 			print 'Grid only, exiting'
 			sys.exit(0)
-	elif algorithm == "wander":
+	elif args.algorithm == "wander":
 		engine = WanderStitch.from_file_names(input_image_file_names)
-	elif algorithm == "all":
+	elif args.algorithm == "all":
 		engine = AllStitch.from_file_names(input_image_file_names)
-	elif algorithm == "fortify":
+	elif args.algorithm == "fortify":
 		if len(input_image_file_names) > 0:
 			raise Exception('Cannot use old project and image files')
 		if input_project_file_name is None:
@@ -207,12 +157,12 @@ if __name__ == "__main__":
 	engine.set_output_project_file_name(output_project_file_name)
 	engine.set_output_image_file_name(output_image_file_name)
 	engine.set_regular(regular)
-	engine.set_dry(dry)
+	engine.set_dry(args.dry)
 	
-	if x_overlap:
-		engine.x_overlap = x_overlap
-	if y_overlap:
-		engine.y_overlap = y_overlap
+	if args.x_overlap:
+		engine.x_overlap = args.x_overlap
+	if args.y_overlap:
+		engine.y_overlap = args.y_overlap
 	
 	if not allow_overwrite:
 		if output_project_file_name and os.path.exists(output_project_file_name):
