4b21df724c3a56790145098bb91bcb40716c8db0
John McMaster <JohnDMcMaster@gmail.com>
2016-11-11 15:02:38 -0800

    pr0nmap: auto name output for sipr0n

--- stitch/pr0nmap.py
+++ stitch/pr0nmap.py
@@ -11,6 +11,7 @@ import argparse
 import datetime
 import multiprocessing
 import os
+import re
 
 std_c_mc = '&copy;%d John McMaster, CC BY' % datetime.datetime.now().year
 std_c_dig = '&copy;%d Digshadow, CC BY' % datetime.datetime.now().year
@@ -20,7 +21,7 @@ if __name__ == "__main__":
     parser.add_argument('images_in', help='image file or dir in')
     parser.add_argument('--level-min', type=int, default=0, help='Minimum zoom level')
     parser.add_argument('--level-max', type=int, default=None, help='Maximum zoom level')
-    parser.add_argument('--out', '-o', dest="out_dir", default="map", help='Output directory')
+    parser.add_argument('--out', '-o', default=None, help='Output directory')
     parser.add_argument('--js-only', action="store_true", dest="js_only", default=False, help='No tiles, only JavaScript')
     parser.add_argument('--skip-missing', action="store_true", dest="skip_missing", default=False, help='Skip missing tiles')
     parser.add_argument('--out-extension', default=None, help='Select output image extension (and type), .jpg, .png, .tif, etc')
@@ -39,7 +40,8 @@ if __name__ == "__main__":
         args.copyright = std_c_dig
 
     im_ext = args.out_extension
-    
+    out_dir = args.out
+
     if os.path.isdir(args.images_in):
         print 'Working on directory of max zoomed tiles'
         source = TileMapSource(args.images_in, threads=args.threads)
@@ -48,6 +50,23 @@ if __name__ == "__main__":
         source = ImageMapSource(args.images_in, threads=args.threads)
         if not im_ext:
             im_ext = '.' + args.images_in.split('.')[-1]
+        # Do auto-magic renaming for standr named die on sipr0n
+        '''
+        Always in form:
+        <vendor>_<chip>_<dataset>.<ext>
+        
+        Typically something like
+        <vendor>_<chip>_<layer>_<mag>.jpg
+        ex: mos_6581r2_mz_mit20x.jpg
+        
+        But also variations
+        ex: mos_6581r2_vec-a.png
+        '''
+        if not out_dir:
+            m = re.match(r'single/([A-Za-z0-9]*)_([A-Za-z0-9\-]*)_(.*)\.(.*)', args.images_in)
+            if m:
+                out_dir = m.group(3)
+                print 'Auto-naming output file for sipr0n: %s' % out_dir
 
     m = Map(source, args.copyright)
     if args.title_name:
@@ -56,9 +75,12 @@ if __name__ == "__main__":
         m.page_title = args.title
     m.min_level = args.level_min
     m.max_level = args.level_max
-    m.out_dir = args.out_dir
     m.js_only = args.js_only
     m.skip_missing = args.skip_missing
+    
+    if not out_dir:
+        out_dir = "map"
+    m.out_dir = out_dir
     
     if not im_ext:
         im_ext = '.jpg'
